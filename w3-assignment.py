777617808da52fa14bb54bd05ab7e1dbe9846b1e
Omomayorwah <kintunde2010@gmail.com>
2025-03-27 10:06:01 +0100

    updated correction to code

--- w3-assignment.py
+++ w3-assignment.py
@@ -1,11 +1,11 @@
 def calculate_discount(price, discount_percent):
-    price = float(input("Price: "))
-    discount_percent = float(input("Discount (%): "))
-    discount = (discount_percent / float(100)) * price
-    final_price = price - discount 
+    discount = (discount_percent / 100) * price
+    final_price = price - discount
     if discount_percent >= 20:
         print("Final Price is: ", final_price)
-    else: print("Final Price is: ", price)
+    else:
+        print("Final Price is: ", price)
 
-calculate_discount(100,20)
-    
+price = float(input("Price: "))
+discount_percent = float(input("Discount (%): "))
+calculate_discount(price, discount_percent)    
